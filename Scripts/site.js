eebb76b14b35cc2e2e2c7fb11dcc87cdafb2a6c8
Wintervacht <stef.kooij@gmail.com>
2017-05-09 21:55:39 +0200

    Initial Commit

---
+++ Scripts/site.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    var $root = $('html, body');
+    $('ul.side-nav > li > a, ul.dropdown-content > li > a').click(function () {
+        $root.animate({
+            scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+        }, 700);
+        $('.button-collapse').sideNav('hide');
+        return false;
+    });
+
+    if ($(window).width() < 600) {
+        $('#vertical').removeClass('valign-wrapper');
+    };
+
+    $(".dropdown-button").dropdown({
+        constrain_width: false,
+        hover: false,
+        belowOrigin: true
+    });
+
+    $('.button-collapse').sideNav({
+        closeOnClick: true
+    });
+});
