fd955a11e1b327be396bbaecfaf75085e93ca652
Wintervacht <stef.kooij@gmail.com>
2017-05-23 17:28:48 +0200

    tuureluurs

--- Scripts/site.js
+++ Scripts/site.js
@@ -1,4 +1,7 @@
 ﻿$(document).ready(function () {
+
+
+
     var $root = $('html, body');
     $(".dropdown-button").dropdown({
         constrain_width: false,
@@ -10,11 +13,44 @@
     });
     $("#content").load("home.html");
     $('.collapsible').collapsible();
-    $("ul.dropdown-content > li > a, ul.side-nav > li > a").on("click", function () {
+    $('ul.dropdown-content > li > a, ul.side-nav > li > a').on("click", function (e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        getContent(href, true);
         $(this).parent().siblings('li').removeClass('active');
         $(this).parent().addClass('active');
-        $("#content").load($(this).attr('href'));
         $('.button-collapse').sideNav('hide');
-        return false;
-    });    
+    });
 });
+
+window.addEventListener("popstate", function (e) {
+    getContent(location.pathname, false);
+    $(function () {
+        var current = location.pathname.replace('/', '');
+        $('ul.dropdown-content > li > a, ul.side-nav > li > a').each(function () {
+            var $this = $(this);            
+            if ($this.attr('href').indexOf(current) !== -1) {
+                $this.parent().addClass('active');
+                if (current == 'index.html') {
+                    $('#loadHome, loadHomem').parent().addClass('active');
+                }
+                $this.parent().siblings('li').removeClass('active');
+            }
+            
+        })
+    });
+});
+
+function getContent(url, addEntry) {
+    $.get(url).done(function (data) {
+        if (url != '/index.html') {
+            $('#content').load(url);            
+        } else if (url == '/index.html') {
+            $('#content').load('home.html');
+        }
+        if (addEntry == true) {
+            history.pushState(null, null, url);
+        }
+    });
+};
+
